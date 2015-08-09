6bb32ba80e5a4bdc79028e615370980cac3991f8
Thomas Cannon <tcannon00@gmail.com>
2015-08-08 21:10:17 -0400

    Add altitude estimation to ground track, improve styling of GT map

--- public/js/ground_track.js
+++ public/js/ground_track.js
@@ -1,7 +1,9 @@
 var GroundTrack = Class.create({
-  initialize: function(datalink, map_id){
+  initialize: function(datalink, map_id, altitudeEstimationId){
     this.datalink = datalink
     this.map_id = map_id
+    this.altitudeEstimationId = altitudeEstimationId
+    this.initializeAltitudeEstimate()
     this.initializeMap()
     this.initializeDatalink()
   },
@@ -13,7 +15,7 @@ var GroundTrack = Class.create({
     var trueAnomalyInRadians = Math.toRadians(trueAnomalyInDegrees) //radians
     var gravitationalParameter = 3531600000000 // Gm/s^2
     var semiMajorAxis = data["o.sma"] //meters
-    var startTime = 0 //seconds
+    this.startTime = 0 //seconds
     var endTime = 0 //seconds
     var inclinationInDegrees = data["o.inclination"] // degrees
     var inclinationInRadians = Math.toRadians(inclinationInDegrees) // radians
@@ -41,13 +43,13 @@ var GroundTrack = Class.create({
     this.GMSTInRadians = OrbitalMath.calculateGMSTInRadiansForOrigin(this.actualPositionVectorInIJK, this.actualLongitudeInRadians)
 
     this.estimatedLatitude = OrbitalMath.findLatitudeOfPositionUnitVector(this.actualPositionVectorInIJK)
-    this.estimatedLongitude = OrbitalMath.findLongitudeOfPositonUnitVector(this.actualPositionVectorInIJK, this.rotationalVelocityOfKerbin, startTime, endTime, this.GMSTInRadians)
+    this.estimatedLongitude = OrbitalMath.findLongitudeOfPositonUnitVector(this.actualPositionVectorInIJK, this.rotationalVelocityOfKerbin, this.startTime, endTime, this.GMSTInRadians)
 
     var E = OrbitalMath.eccentricAnomalyFromTrueAnomalyAndEcentricity(trueAnomalyInRadians, eccentricity)
 
     this.orbitalPredictionValues = []
 
-    var startTime = Math.sqrt(Math.pow(semiMajorAxis,3)/gravitationalParameter) * (E - eccentricity * Math.sin(E))
+    this.startTime = Math.sqrt(Math.pow(semiMajorAxis,3)/gravitationalParameter) * (E - eccentricity * Math.sin(E))
     var endOfPlot = Math.toDegrees(E) + 720
 
     var lastLatitude = null
@@ -66,7 +68,7 @@ var GroundTrack = Class.create({
 
       var estimatedPositionVectorInIJK = OrbitalMath.transformPositionPQWVectorToIJKFrame(estimatedPositionVectorInPQW, inclinationInRadians, longitudeOfAscendingNodeInRadians, argumentOfPeriapsisInRadians)
       var latitudeInDegrees = Math.toDegrees(OrbitalMath.findLatitudeOfPositionUnitVector(estimatedPositionVectorInIJK))
-      var longitudeInDegrees = Math.toDegrees(OrbitalMath.findLongitudeOfPositonUnitVector(estimatedPositionVectorInIJK, this.rotationalVelocityOfKerbin, startTime, endTime, this.GMSTInRadians))
+      var longitudeInDegrees = Math.toDegrees(OrbitalMath.findLongitudeOfPositonUnitVector(estimatedPositionVectorInIJK, this.rotationalVelocityOfKerbin, this.startTime, endTime, this.GMSTInRadians))
       var altitude = Math.sqrt(Math.pow(estimatedPositionVectorInPQW.p,2) + Math.pow(estimatedPositionVectorInPQW.q,2) + Math.pow(estimatedPositionVectorInPQW.w,2))
 
 
@@ -92,11 +94,13 @@ var GroundTrack = Class.create({
       this.orbitalPredictionValues.push({
         altitude: altitude,
         latitude: latitudeInDegrees,
-        longitude: longitudeInDegrees
+        longitude: longitudeInDegrees,
+        time: endTime
       })
     }
 
     this.updateMap()
+    this.updateAltitudeEstimateChart()
   },
 
   convertCoordinatesToMap: function(latitude, longitude){
@@ -119,7 +123,7 @@ var GroundTrack = Class.create({
     var orbitalPredictionSets = []
 
     var previousOrbitalPredictionValue = null
-    for (var i = 0 ; i < this.orbitalPredictionValues.length; i++) {
+    for (var i = 0 ; i < this.orbitalPredictionValues.length; (10 * i++)) {
       var orbitalPredictionValue = this.orbitalPredictionValues[i]
       var latitude = orbitalPredictionValue.latitude
       var longitude = orbitalPredictionValue.longitude
@@ -145,7 +149,7 @@ var GroundTrack = Class.create({
     for (var i = 0; i < orbitalPredictionSets.length; i++) {
       var coordinateSet = orbitalPredictionSets[i]
       if(!this.markers.orbitalPaths[i]){
-        this.markers.orbitalPaths[i] = L.polyline([], {color: 'red'})
+        this.markers.orbitalPaths[i] = L.polyline([], {color: '#F5A623'})
         this.markers.orbitalPaths[i].addTo(this.map)
       }
 
@@ -154,6 +158,33 @@ var GroundTrack = Class.create({
 
     var estimatedCoordinates = this.orbitalPredictionValues[0]
     this.setCoordinatesForMapObject(this.markers.estimatedCoordinates, estimatedCoordinates.latitude, estimatedCoordinates.longitude)
+  },
+
+  updateAltitudeEstimateChart: function(){
+    var chartData = {labels: [], series: [[]]}
+    var interval = 60 * 5 //seconds based
+    var intervalsCovered = {}
+
+    for (var i = 0 ; i < this.orbitalPredictionValues.length; (interval * i++)) {
+      var orbitalPredictionValue = this.orbitalPredictionValues[i]
+      var deltaT = orbitalPredictionValue.time - this.startTime
+
+      var intervalPeriod = Math.floor(deltaT/interval)
+
+      if(!intervalsCovered[intervalPeriod]){
+        if(intervalPeriod != 0){
+          var label = "-" + TimeFormatters.durationString(deltaT.toFixed(0))
+        } else{
+          var label = ""
+        }
+
+        chartData.labels.push(label)
+        chartData.series[0].push(orbitalPredictionValue.altitude)
+        intervalsCovered[intervalPeriod] = true
+      }
+    }
+
+    this.altitudeEstimateChart.update(chartData)
   },
 
   initializeDatalink: function(){
@@ -170,8 +201,7 @@ var GroundTrack = Class.create({
   initializeMap: function(){
     this.map = new L.KSP.Map(this.map_id, {
       layers: [L.KSP.CelestialBody.KERBIN],
-      zoom: L.KSP.CelestialBody.KERBIN.defaultLayer.options.maxZoom,
-      center: [-0.1027, -74.5754],
+      zoom: 'fit',
       bodyControl: false,
       layerControl: true,
       scaleControl: true
@@ -199,16 +229,39 @@ var GroundTrack = Class.create({
       popupAnchor:  [-1, -34] // point from which the popup should open relative to the iconAnchor
     })
 
+    var circleMarkerOptions = {
+      // fill: false,
+      color: '#FD7E23',
+      opacity: 1.0,
+      fillOpacity: 1.0,
+      radius: 5
+    }
 
     this.markers = {
-      actualCoordinates : L.marker([0, 0]),
-      estimatedCoordinates : L.marker([0,0], {icon: estimationIcon}),
-      convertedActualCoordinates : L.marker([0,0], {icon: conversionIcon}),
+      actualCoordinates : L.circleMarker([0, 0], circleMarkerOptions),
+      estimatedCoordinates : L.circleMarker([0,0], circleMarkerOptions),
+      convertedActualCoordinates : L.circleMarker([0,0], circleMarkerOptions),
       orbitalPaths : []
     }
 
     this.markers.actualCoordinates.addTo(this.map)
     this.markers.estimatedCoordinates.addTo(this.map)
     this.markers.convertedActualCoordinates.addTo(this.map)
+  },
+
+  initializeAltitudeEstimate: function(){
+    var data = {
+      // A labels array that can contain any sort of values
+      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      // Our series array that contains series objects or in this case series data arrays
+      series: [
+        [5, 2, 4, 2, 0]
+      ]
+    };
+
+    // Create a new line chart object where as first parameter we pass in a selector
+    // that is resolving to our chart container element. The Second parameter
+    // is the actual data object.
+    this.altitudeEstimateChart = new Chartist.Line("#" + this.altitudeEstimationId, data);
   }
 })
