0d898187e58e286c85b6a45cba84fb124d7c24e4
Thomas Cannon <tcannon00@gmail.com>
2015-08-15 12:24:40 -0400

    add basic pulldown navigation

--- lib/default.rb
+++ lib/default.rb
@@ -5,3 +5,8 @@ include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+def current_page?(item, filename)
+  return if filename.nil?
+  item[:filename].end_with?("#{filename}.html")
+end
