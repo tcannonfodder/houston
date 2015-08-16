0b5681b40ff2e85da67c9f8ea4bbadc0811eeb77
Thomas Cannon <tcannon00@gmail.com>
2015-08-15 22:16:41 -0400

    add orbiting body info table

--- static/js/data_formatters.js
+++ static/js/data_formatters.js
@@ -14,5 +14,25 @@ var DataFormatters = {
 
   velocityString: function(value){
     return numeral(value).format('0,0.000 a') + "m/s"
-  }
+  },
+
+  temperatureString: function(value){
+    if(!value){return "NA"}
+    return numeral(value).format('0,000') + "&deg;C"
+  },
+
+  accelerationSensorString: function(value){
+    if(value[0] == "No Sensors of the Appropriate Type"){return "NA"}
+    return numeral(value[1][0]).format('0,000') + "G"
+  },
+
+  pressureSensorString: function(value){
+    if(value[0] == "No Sensors of the Appropriate Type"){return "NA"}
+    return numeral(value[1][0]).format('0,000') + "Pa"
+  },
+
+  gravitySensorString: function(value){
+    if(value[0] == "No Sensors of the Appropriate Type"){return "NA"}
+    return numeral(value[1][0]).format('0,000 a') + "m/s&sup2;"
+  },
 }
