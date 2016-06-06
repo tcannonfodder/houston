2e1bf674f20918e77e9cb3e600d11b503b0d4fc7
Thomas Cannon <tcannon00@gmail.com>
2016-06-06 18:38:45 -0400

    Replace mission time with LOS warning if the event is fired

--- static/js/title_bar.js
+++ static/js/title_bar.js
@@ -3,6 +3,7 @@ var TitleBar = Class.create({
     this.datalink = datalink
     this.title_bar_id = title_bar_id
     this.title_bar = $(this.title_bar_id)
+    this.initializeLOSNotifier()
     this.initializeDatalink()
   },
 
@@ -10,6 +11,17 @@ var TitleBar = Class.create({
     window.requestAnimationFrame(function(){
       this.title_bar.down("#world-clock").update(TimeFormatters.formatUT(data["t.universalTime"]))
       this.title_bar.down("#mission-time").update(TimeFormatters.formatMET(data["v.missionTime"]))
+
+      this.title_bar.down("#mission-time").removeClassName("loss-of-signal")
+    }.bind(this))
+  },
+
+  initializeLOSNotifier:function(){
+    document.observe('telemachus:loss-of-signal', function(){
+      window.requestAnimationFrame(function(){
+        this.title_bar.down("#mission-time").update("&#9888; LOS &#9888;")
+        this.title_bar.down("#mission-time").addClassName("loss-of-signal")
+      }.bind(this))
     }.bind(this))
   },
 
